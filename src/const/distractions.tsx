b5b980b5cd19294957c96c03ce93f1758c0545e5
mraheja <mraheja@berkeley.edu>
2024-03-30 15:52:10 -0700

    remove endless horse

--- src/const/distractions.tsx
+++ src/const/distractions.tsx
@@ -6,9 +6,6 @@ export const DISTRACTIONS = (onClose: () => void) => {
     <DistractionModal onClose={onClose} name="Cat bounce" key={1}>
       <iframe src="https://cat-bounce.com/" className="w-full h-full" />
     </DistractionModal>,
-    <DistractionModal onClose={onClose} name="Endless Horse" key={2}>
-      <iframe src="http://endless.horse/" className="w-full h-full" />
-    </DistractionModal>,
     <DistractionModal onClose={onClose} name="Pointer Pointer" key={3}>
       <iframe src="https://pointerpointer.com/" className="w-full h-full" />
     </DistractionModal>,
