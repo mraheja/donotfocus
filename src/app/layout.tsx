d8914db20bde8d8a92800c4ae6fd830f91056f32
mraheja <mraheja@berkeley.edu>
2024-03-30 18:13:46 -0700

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Credits } from "@/components/Credits/Credits";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>{children}</body>
+      <Credits />
     </html>
   );
 }
---
+++ src/components/Credits/Credits.tsx
@@ -0,0 +1,8 @@
+export const Credits = () => {
+    return (
+        <div className="absolute bottom-3 left-3 flex flex-col text-xs justify-center items-center p-2 border-black border rounded-md">
+            <span className="italic">Made by</span>
+            <span className="font-semibold">Mehul Raheja</span>
+        </div>
+    );
+};
